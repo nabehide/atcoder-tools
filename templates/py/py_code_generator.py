cb2f07d213a300e2ce92428556174448d51820cf
nabehide <iasoyv@gmail.com>
2017-11-11 02:58:33 +0900

    add comment in templates/py/py_code_generator.py

--- templates/py/py_code_generator.py
+++ templates/py/py_code_generator.py
@@ -6,19 +6,11 @@ from core.TemplateEngine import render
 mydir = os.path.dirname(__file__)
 
 
-def formatInformation(information):
-    return information.split('\n')[:-1]
-
-
-def formatSamples(samples):
-    # lst = []
-    # for s in samples:
-    #     lst.append(s[0].split('\n'))
-    # return lst
-    return samples[0][0].split('\n')[:-1]
-
-
 def formatInfo(info, ignore):
+    """ Add variables into "info" if the variable is not in "ignore".
+        When a variable is added into "info", it's also added  into "ignore"
+        to avoid duplication.
+    """
     lst = info.split(' ')
     for i, l in enumerate(lst):
         idx = l.find('_')
@@ -29,72 +21,114 @@ def formatInfo(info, ignore):
     return lst, ignore
 
 
+def generate_input_part(information, samples):
+    input_part_lines = ""
+    ignore = ['...', ':']
+    splittedInfo = information.split('\n')[:-1]
+    splittedSamples = samples[0][0].split('\n')[:-1]
+
+    for idx, (info, sample) in enumerate(zip(splittedInfo, splittedSamples)):
+        formattedInfo = info.split(' ')
+        formattedSample = sample.split(' ')
+        isInt = formattedSample[0].isdigit()
+
+        line = ""
+        if 1 == len(formattedInfo) and formattedInfo[0] not in ignore:
+            """ In case one variable is in one line.
+                e.g.
+                  A
+            """
+            line += formattedInfo[0] + " = "
+            line += "int(input())\n" if isInt else "list(input())\n"
+        else:
+            formattedInfo, ignore = formatInfo(info, ignore)
+            if len(formattedInfo) == 0:
+                """ In case no variable is in one line (e.g. ":")
+                    or the variables have already be processed.
+                """
+                pass
+            elif formattedInfo == [formattedInfo[0]] * len(formattedInfo):
+                """ In case 1 or 2 dimensional variable is in one line.
+                    e.g.
+                      A_1, ..., A_N
+                        or
+                      A_11, ..., A_1N
+                      :
+                      A_M1, ..., A_MN
+                """
+                if (idx != len(splittedInfo) - 1) and \
+                        splittedInfo[idx + 1][0] == ':':
+                    """ In case 2 dimensional variable is in several lines.
+                        e.g.
+                          A_11, ..., A_1N
+                          :
+                          A_M1, ..., A_MN
+                    """
+                    if isInt:
+                        line += formattedInfo[0] + \
+                                " = [list(map(int, input().split())) " + \
+                                "for i in range(" + 'N' + ")]\n"
+                    else:
+                        line += formattedInfo[0] + " = [list(input()) " + \
+                                "for i in range(" + 'N' + ")]\n"
+                else:
+                    """ In case 1 dimensional variable is in one line.
+                        e.g.
+                          A_1, ..., A_N
+                    """
+                    if isInt:
+                        line += formattedInfo[0] + \
+                                " = list(map(int, input().split()))\n"
+                    else:
+                        line += formattedInfo[0] + " = list(input())\n"
+            elif (idx != len(splittedInfo) - 1) and \
+                    splittedInfo[idx + 1][0] == ':':
+                """ In case 1 dimensional variables are in several lines.
+                    e.g.
+                      A_1, B_1, ...
+                      :
+                      A_N, B_N, ...
+                """
+                for i in formattedInfo:
+                    line += i + " = []\n"
+                line += "for i in range(" + 'N' + "):\n"
+                for i in range(len(formattedInfo)):
+                    line += "temp" + str(i)
+                    if i != len(formattedInfo) - 1:
+                        line += ", "
+                if isInt:
+                    line += " = list(map(int, input().split()))\n"
+                else:
+                    line += " = list(input())\n"
+                for i, f in enumerate(formattedInfo):
+                    line += "    "
+                    line += f + ".append(temp" + str(i) + ")\n"
+            else:
+                """ In case several numbers are in one line.
+                    e.g.
+                      A, B, ...
+                """
+                for i, info in enumerate(formattedInfo):
+                    line += info
+                    if i != len(formattedInfo) - 1:
+                        line += ", "
+                line += " = list(map(int, input().split()))\n"
+
+        input_part_lines += line
+
+    return input_part_lines
+
+
 def code_generator(information=None, samples=None):
     with open("{dir}/template_success.py".format(dir=mydir), "r") as f:
         template_success = f.read()
     with open("{dir}/template_failure.py".format(dir=mydir), "r") as f:
         template_failure = f.read()
 
-    ignore = ['...', ':']
-
     if information is not None:
-
-        information = formatInformation(information)
-        samples = formatSamples(samples)
-
-        input_part_lines = ""
-        for idx, (info, sample) in enumerate(zip(information, samples)):
-            formattedInfo = info.split(' ')
-            formattedSample = sample.split(' ')
-            isVariableInt = formattedSample[0].isdigit()
-
-            # if len(formattedInfo) == len(formattedSample):
-            line = ""
-            if 1 == len(formattedInfo) and formattedInfo[0] not in ignore:
-                line += formattedInfo[0] + " = "
-                line += "int(input())\n" if isVariableInt else "list(input())\n"
-            else:
-                formattedInfo, ignore = formatInfo(info, ignore)
-                if len(formattedInfo) == 0:
-                    pass
-                elif formattedInfo == [formattedInfo[0]] * len(formattedInfo):
-                    if (idx != len(information) - 1) and information[idx + 1][0] == ':':
-                        if isVariableInt:
-                            line += formattedInfo[0] + " = [list(map(int, input().split())) for i in range(" + 'N' + ")]\n"
-                        else:
-                            line += formattedInfo[0] + " = [list(input()) for i in range(" + 'N' + ")]\n"
-                    else:
-                        if isVariableInt:
-                            line += formattedInfo[0] + " = list(map(int, input().split()))\n"
-                        else:
-                            line += formattedInfo[0] + " = list(input())\n"
-                elif (idx != len(information) - 1) and information[idx + 1][0] == ':':
-                    for i in formattedInfo:
-                        line += i + " = []\n"
-                    line += "for i in range(" + 'N' + "):\n"
-                    for i in range(len(formattedInfo)):
-                        line += "temp" + str(i)
-                        if i != len(formattedInfo) - 1:
-                            line += ", "
-                    if isVariableInt:
-                        line += " = list(map(int, input().split()))\n"
-                    else:
-                        line += " = list(input())\n"
-                    for i, f in enumerate(formattedInfo):
-                        line += "    "
-                        line += f + ".append(temp" + str(i) + ")\n"
-                else:
-                    for i, info in enumerate(formattedInfo):
-                        line += info
-                        if i != len(formattedInfo) - 1:
-                            line += ", "
-                    line += " = list(map(int, input().split()))\n"
-
-            input_part_lines += line
-            ignore.append('')
-
+        input_part_lines = generate_input_part(information, samples)
         code = render(template_success,
                       input_part=input_part_lines)
     else:
-        code = template_failure
+        code = render(template_failure)
     return code
